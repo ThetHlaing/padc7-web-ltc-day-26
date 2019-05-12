e56ab8df745806719b54f4723bf7f8a6fcc4bc3a
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-05-12 12:19:04 +0630

    EOD

--- routes/fruits.js
+++ routes/fruits.js
@@ -4,7 +4,9 @@ const route = express.Router();
 
 const sql = require('../utilities/mysql');
 
+const fruitModel = require('../models/fruits');
 
+const Joi = require('@hapi/joi');
 
 route.get('/', (req, res) => {
     sql.query("Select * from fruits", (err, data) => {
@@ -14,7 +16,7 @@ route.get('/', (req, res) => {
         }
         else {
             console.log(data);
-            res.render('fruits', { data: data });
+            res.render('fruit/index', { data: data });
         }
     });
 
@@ -41,6 +43,74 @@ route.post('/', (req, res) => {
                 res.redirect('/fruits');
             }
         })
+})
+
+route.get('/:id/delete', (req, res) => {
+
+    fruitModel.getFruitById(req.params.id)
+        .then(data => {
+            console.log(data);
+            res.render('fruit/delete', { data: data });
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("Unexcepted error occured");
+        });
+})
+
+route.post('/:id/delete', (req, res) => {
+    fruitModel.deleteById(req.params.id)
+        .then(data => {
+            console.log("Delete result", data);
+            res.redirect('/fruits');
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("Unexcepted error occured");
+        })
+
+});
+
+route.get('/:id/update', (req, res) => {
+    fruitModel.getFruitById(req.params.id)
+        .then(data => {
+            res.render('fruit/update', { data: data });
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("Unexcepted error occured");
+        })
+})
+
+
+route.post('/:id/update', (req, res) => {
+
+    const schema = Joi.object().keys({
+        fruit: Joi.string().required(),
+        stock: Joi.number().required()
+    })
+
+    Joi.validate(req.body, schema, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send("Error");
+        }
+        else {
+            fruitModel.updateById(req.params.id, {
+                name: req.body.fruit,
+                stock: req.body.stock
+            })
+                .then(data => {
+                    console.log(data);
+                    res.redirect("/fruits");
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.send("Unexcepted error occured");
+                })
+        }
+    })
+
 })
 
 module.exports = route;
--- utilities/mysql.js
+++ utilities/mysql.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
 
+const env = require('dotenv');
+env.config();
+
 const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'admin',
-    password : '123456',
-    database : 'mysqltest'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 })
 
 connection.connect((err)=>{
